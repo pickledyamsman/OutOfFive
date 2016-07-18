11ddd9e6e023124850f260cccbb951a49d524371
pickledyamsman <pickledyamsman@gmail.com>
2016-07-18 12:25:25 -0400

    sign in and sign up direct to profile page. Navbar fully functional.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  root 'welcome#index'
+
+  authenticated :user do
+    root to: "users#index", as: :authenticated_root
+  end
+
+  unauthenticated do
+    root to: "welcome#index", as: :unauthenticated_root
+  end
 end
